4149010cb9e595d8956d88eb74c8623af0158d81
Jado <jade.bouiges@edu.esiee-it.fr>
2024-09-29 16:33:51 +0200

    add colisions between tetrominos

--- Assets/TetrisBlock.cs
+++ Assets/TetrisBlock.cs
@@ -8,8 +8,14 @@ public class TetrisBlock : MonoBehaviour
     private float _previousTime;
     public float fallTime = 0.8f;
     public static int height = 20;
-    public static int width = 10;
-    [SerializeField] GameObject _rotatePoint;
+    public static int width = 12;
+    [SerializeField] bool canTotalyTurn;
+    [SerializeField] private bool primary = true;
+    [SerializeField] private bool canTurn = true;
+    [SerializeField] private bool cLaS;
+    [SerializeField] private bool cLaL;
+
+    private static Transform[,] grid = new Transform[width, height];
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -25,12 +31,60 @@ public class TetrisBlock : MonoBehaviour
 
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && canTurn)
         {
-            transform.RotateAround(_rotatePoint.transform.TransformPoint(rotationPoint), new Vector3(_rotatePoint.transform.rotation.x, _rotatePoint.transform.rotation.y, 1), 90);
-            if(!ValidMove())
+            if (!canTotalyTurn)
             {
-                transform.RotateAround(_rotatePoint.transform.TransformPoint(rotationPoint), new Vector3(_rotatePoint.transform.rotation.x, _rotatePoint.transform.rotation.y, 1), -90);
+                if (primary)
+                {
+                    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+                    primary = false;
+                }
+                else if (!primary)
+                {
+
+                    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                    primary = true;
+                }
+
+            }
+            if (canTotalyTurn)
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+
+            }
+            if (!ValidMove())
+            {
+                if (!cLaS && !canTotalyTurn)
+                {
+                    if (primary)
+                    {
+                        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                    }
+                    else
+                    {
+                        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+                    }
+
+                }
+
+                if (cLaS)
+                {
+                    if (primary)
+                    {
+                        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+                    }
+                    else
+                    {
+                        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                    }
+
+                }
+
+                if (canTotalyTurn)
+                {
+                    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+                }
 
             }
         }
@@ -39,7 +93,25 @@ public class TetrisBlock : MonoBehaviour
         {
             transform.position += new Vector3(0, -1, 0);
 
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                this.enabled = false;
+                FindObjectOfType<SpawnTetromino>().NewTetromino();
+            }
             _previousTime = Time.time;
+        }
+    }
+
+    private void AddToGrid()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
         }
     }
 
@@ -49,7 +121,28 @@ public class TetrisBlock : MonoBehaviour
         int roundedX = Mathf.RoundToInt(children.transform.position.x);
         int roundedY = Mathf.RoundToInt(children.transform.position.y);
 
-            if(roundedX < 0 || roundedX >= width || roundedY >= height)
+            if (!canTotalyTurn || cLaL)
+            {
+                if (roundedX <= 0 || roundedX >= width)
+                {
+                    return false;
+                }
+            }
+
+            if (canTotalyTurn && !cLaL)
+            {
+                if ((roundedX <= 1.25f || roundedX >= 10.75f))
+                {
+                    return false;
+                }
+            }
+
+            if (roundedY <= 0f)
+            {
+                return false;
+            }
+
+            if (grid[roundedX,roundedY] != null)
             {
                 return false;
             }
